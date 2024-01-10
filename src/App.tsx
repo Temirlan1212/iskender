45a782359d95cd3262437655440dfdfe99fd9a2b
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2024-01-10 19:05:47 +0600

    adding: card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from "./components/cards/card";
 import Navbar from "./components/layouts/navbar";
 import { CategoryList } from "./components/pages/home/category/category-list";
 import { ReleventList } from "./components/pages/home/relevant/relevant-list";
@@ -14,6 +15,20 @@ function App() {
           <div className="flex flex-col gap-[10px]">
             <h2 className="font-semibold text-[25px]">Категории</h2>
             <CategoryList />
+          </div>
+
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="font-semibold text-[25px]">Категории</h2>
+            <Card
+              url={"https://github.com/shadcn.png"}
+              title={""}
+              quantity={0}
+              props={{
+                img: {
+                  className: "rounded-[10px]",
+                },
+              }}
+            />
           </div>
         </div>
       </div>
--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -1,4 +1,6 @@
+import { Heart, ShoppingCart } from "lucide-react";
 import { cn } from "../../lib/utils";
+import { Button } from "../ui/button";
 
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   url: string;
@@ -13,24 +15,33 @@ const Card = ({ url, title, quantity, className, props }: CardProps) => {
   return (
     <div
       className={cn(
-        "flex flex-col w-[fit-content] gap-[5px] w-[200px] h-[341px] bg-white rounded-[10px]",
+        "flex flex-col w-[fit-content] gap-[5px] w-[200px] h-[341px] bg-white rounded-[10px] p-[10px]",
         className
       )}
     >
+      <div className="relative">
+        <Button
+          variant="outline"
+          size="icon"
+          className="absolute top-[5px] right-[5px] bg-white/60 border-0 hover:bg-white/60"
+        >
+          <Heart color="#717171" />
+        </Button>
+        <img
+          src={url}
+          alt={title}
+          {...(props?.img || {})}
+          className={cn(
+            "w-full h-[150px] object-cover !border-0",
+            props?.img?.className || ""
+          )}
+        />
+      </div>
+
       <div className="p-[10px]">
         <p className="font-semibold">{title}</p>
         <p className="text-[#727272]">{quantity} товаров</p>
       </div>
-
-      <img
-        src={url}
-        alt={title}
-        {...(props?.img || {})}
-        className={cn(
-          "w-[170px] h-[107px] object-cover !border-0",
-          props?.img?.className || ""
-        )}
-      />
     </div>
   );
 };
